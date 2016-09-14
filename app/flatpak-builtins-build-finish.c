c282ae0d19d9a66a46b2b8f3b887d6306e7356a3
Mario Sanchez Prada <mario@endlessm.com>
2016-09-14 11:54:43 +0100

    build-finish: also export speedwagon files
    This will allow to ship them together with application bundles, instead
    of the core OS.

--- app/flatpak-builtins-build-finish.c
+++ app/flatpak-builtins-build-finish.c
@@ -189,6 +189,7 @@ collect_exports (GFile *base, const char *app_id, gboolean is_runtime, GCancella
     "share/icons/hicolor",                /* Icons */
     "share/dbus-1/services",              /* D-Bus service files */
     "share/gnome-shell/search-providers", /* Search providers */
+    "share/eos-shell-content/splash",     /* Endless splash screens */
     NULL,
   };
   const char *runtime_paths[] = {
