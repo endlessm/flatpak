e4d9c210ac4e37a0f9893157890a8ed3e14dcbb6
Mario Sanchez Prada <mario@endlessm.com>
2016-09-20 01:49:43 +0000

    WIP: Playing with the public API

--- lib/flatpak-installation.c
+++ lib/flatpak-installation.c
@@ -61,6 +61,7 @@ struct _FlatpakInstallationPrivate
      flatpak_installation_drop_caches(), so every user needs to keep its own reference alive until
      done. */
   FlatpakDir *dir_unlocked;
+  gboolean is_custom;
 };
 
 G_DEFINE_TYPE_WITH_PRIVATE (FlatpakInstallation, flatpak_installation, G_TYPE_OBJECT)
@@ -124,6 +125,9 @@ flatpak_installation_class_init (FlatpakInstallationClass *klass)
 static void
 flatpak_installation_init (FlatpakInstallation *self)
 {
+  FlatpakInstallationPrivate *priv = flatpak_installation_get_instance_private (self);
+
+  priv->is_custom = FALSE;
 }
 
 static FlatpakInstallation *
@@ -225,9 +229,20 @@ flatpak_installation_new_for_path (GFile *path, gboolean user,
                                    GCancellable *cancellable,
                                    GError **error)
 {
+  g_autofree gchar *path_string = NULL;
+  FlatpakInstallation *installation = NULL;
+  FlatpakInstallationPrivate *priv = NULL;
+
   flatpak_migrate_from_xdg_app ();
 
-  return flatpak_installation_new_for_dir (flatpak_dir_new (path, user), cancellable, error);
+  path_string = g_file_get_path (path);
+  flatpak_set_custom_installation_path (path_string);
+
+  installation = flatpak_installation_new_for_dir (flatpak_dir_get_custom (), cancellable, error);
+  priv = flatpak_installation_get_instance_private (installation);
+  priv->is_custom = TRUE;
+
+  return installation;
 }
 
 static FlatpakDir *
