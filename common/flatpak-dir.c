908f9b6fce8cdacd61ed7a0ca8e2b5997b4b9e20
Sam Spilsbury <sam@endlessm.com>
2017-03-28 14:48:54 +0800

    build-finish: Add Endless Discovery Feed Providers
    https://phabricator.endlessm.com/T16446
    
    Rebase 0.11.3 (T21153): Update to use the new
      BuiltinExportedDirectoryInfo structure
    
    Rebase 0.11.6: Update to use the new way of adding export directories
      (added in commit 8f428fd76 upstream)
    
    Rebase 1.1.3 (T25194): Use strcmp() rather than
      flatpak_has_path_prefix() in flatpak_get_allowed_exports() to match
      what's being done for desktop files, etc.

--- common/flatpak-dir.c
+++ common/flatpak-dir.c
@@ -7111,6 +7111,7 @@ flatpak_export_dir (GFile        *source,
     "share/mime/packages",                 "../../..",
     "bin",                                 "..",
     "share/eos-shell-content/splash",      "../../..",
+    "share/eos-discovery-feed/content-providers", "../../..",
   };
   int i;
 
