b0232b42c737aaa99e93ef260714543c71f2f7e6
Joaquim Rocha <jrocha@endlessm.com>
2017-12-11 10:55:14 +0000

    tests: Make the app ID in make-test-app.sh a parameter
    This is in order to allow tests to make several apps if needed, and will
    be useful when e.g. testing repos that have multiple apps with different
    collection IDs.
    
    This patch modifies the mentioned script as mentioned and updates the
    places that call it.

--- tests/testlibrary.c
+++ tests/testlibrary.c
@@ -636,7 +636,7 @@ static void
 make_test_app (void)
 {
   g_autofree char *arg0 = NULL;
-  char *argv[] = { NULL, "test", "", NULL };
+  char *argv[] = { NULL, "test", "", "", NULL };
 
   arg0 = g_test_build_filename (G_TEST_DIST, "make-test-app.sh", NULL);
   argv[0] = arg0;
