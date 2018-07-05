5922e39b94e896c5b89781e66fe7c78ba723ae65
Robert McQueen <rob@endlessm.com>
2018-07-05 15:48:11 +0100

    profile/flatpak.sh: add export/bin dirs to PATH
    Add the com.id.foo wrappers which are created by flatpak in the
    export/bin dir to the default PATH.
    
    https://phabricator.endlessm.com/T22334
    
    Rebase 1.1.3 (T25194): Rewrite to match the way flatpak.sh was recently
      rewritten to modify XDG_DATA_DIRS
    
    Rebase 1.3.3 (T26434): Resolve minor conflicts to incorporate a change
      from upstream (684b19e719)

--- profile/flatpak.sh
+++ profile/flatpak.sh
@@ -23,4 +23,28 @@ if command -v flatpak > /dev/null; then
 
     export XDG_DATA_DIRS
     XDG_DATA_DIRS="${new_dirs:+${new_dirs}:}${XDG_DATA_DIRS:-/usr/local/share:/usr/share}"
+
+    # Set PATH to include Flatpak launch scripts
+
+    new_dirs=$(
+        (
+            echo "${XDG_DATA_HOME:-"$HOME/.local/share"}/flatpak"
+            flatpak --installations
+        ) | (
+            new_dirs=
+            while read -r install_path
+            do
+                bin_path=$install_path/exports/bin
+                case ":$PATH:" in
+                    (*":$bin_path:"*) :;;
+                    (*":$bin_path/:"*) :;;
+                    (*) new_dirs=${new_dirs:+${new_dirs}:}$bin_path;;
+                esac
+            done
+            echo "$new_dirs"
+        )
+    )
+
+    export PATH
+    PATH="${PATH:-/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games}${new_dirs:+:${new_dirs}}"
 fi
